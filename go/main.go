14a5f5528c0cdab7bdf7763d8f838273cd3151d1
gongon84 <shinya7102@gmail.com>
2023-09-21 14:38:23 +0900

    add 足し算をブラウザに表示

--- go/main.go
+++ go/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/gongon84/learning-golangci/go/domains"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
+		return c.String(http.StatusOK, "1 + 4 = "+strconv.Itoa(domains.Add(1, 4)))
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
