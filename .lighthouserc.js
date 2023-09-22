3db7b6900fb3a08b90db7106ad4f23d06aa2e3b4
gongon84 <shinya7102@gmail.com>
2023-09-22 12:58:13 +0900

    fix assertコマンドをなくす

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,9 +4,6 @@ module.exports = {
       url: ['http://localhost:8080/'],
       startServerCommand: 'go run ./go/main.go',
     },
-    assert: {
-      assertions: {}
-    },
     upload: {
       target: 'temporary-public-storage',
     },
